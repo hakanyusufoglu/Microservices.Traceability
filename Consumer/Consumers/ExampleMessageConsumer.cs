f2dd800a9fe2d44f935e13256c4a0f6b65e3bf42
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-06-06 14:56:59 +0300

    Servisler arası traceability gerçekleştirmek adına consumer tamamlandı. Böylelikle servisler arası traceability simüle edildi

---
+++ Consumer/Consumers/ExampleMessageConsumer.cs
@@ -0,0 +1,24 @@
+﻿using MassTransit;
+using Shared;
+using System.Diagnostics;
+
+namespace Consumer.Consumers
+{
+    // Bu sınıfın amacı, ExampleMessage tipindeki mesajları dinlemek ve işlemektir.
+    public class ExampleMessageConsumer(ILogger<ExampleMessageConsumer> logger) : IConsumer<ExampleMessage>
+    {
+        public Task Consume(ConsumeContext<ExampleMessage> context)
+        {
+            var correlationId = Guid.NewGuid();
+            if(context.Headers.TryGetHeader("CorrelationId", out var _correlationId))
+                correlationId = Guid.Parse(_correlationId.ToString());
+
+            Trace.CorrelationManager.ActivityId = correlationId;
+            logger.LogDebug("Consumer Log");
+
+            Console.WriteLine($"Receive message : {context.Message.Text} - Correlation Id : {correlationId}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,6 +1,29 @@
 using Consumer;
+using Consumer.Consumers;
+using MassTransit;
+using NLog.Extensions.Logging;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddMassTransit(configurator =>
+{
+    configurator.AddConsumer<ExampleMessageConsumer>();
+
+    configurator.UsingRabbitMq((context, _configurator) =>
+    {
+        //appsettings.json dan alýnmalý
+        _configurator.Host("amqps://cgjbwxmq:GikU8sJjItWnokq2HTm2MYwvpOIDP-5V@cow.rmq2.cloudamqp.com/cgjbwxmq");
+
+        _configurator.ReceiveEndpoint("example-message-qeueue", endpoint =>
+        {
+            endpoint.ConfigureConsumer<ExampleMessageConsumer>(context);
+        });
+    });
+});
+
+builder.Logging.ClearProviders();
+builder.Logging.AddNLog();
+
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
