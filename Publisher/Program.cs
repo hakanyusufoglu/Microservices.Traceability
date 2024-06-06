800f077c8e226ab5771aa87a3010566005f5ee9a
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-06-06 14:44:35 +0300

    Publisher işleminde traceability uygulaması gerçekleştirildi.

--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -1,7 +1,26 @@
-using Publisher;
-
+using MassTransit;
+using NLog.Extensions.Logging;
+using Publisher.Services;
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+builder.Services.AddMassTransit(configurator =>
+{
+    configurator.UsingRabbitMq ((context, _configurator) =>
+    {
+        //appsettings.json dan alýnmalý
+        _configurator.Host("amqps://cgjbwxmq:GikU8sJjItWnokq2HTm2MYwvpOIDP-5V@cow.rmq2.cloudamqp.com/cgjbwxmq");
+    });
+});
 
+builder.Logging.ClearProviders();
+builder.Logging.AddNLog();
+
+builder.Services.AddHostedService<PublishMessageService>(provider =>
+{
+    using IServiceScope scope = provider.CreateScope();
+    IPublishEndpoint publishEndpoint = scope.ServiceProvider.GetService<IPublishEndpoint>();
+
+    var logger = scope.ServiceProvider.GetService<ILogger<PublishMessageService>>();
+    return new(publishEndpoint, logger);
+});
 var host = builder.Build();
 host.Run();
---
+++ Publisher/Services/PublishMessageService.cs
@@ -0,0 +1,42 @@
+﻿using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Publisher.Services
+{
+    public class PublishMessageService(IPublishEndpoint publishEndpoint, ILogger<PublishMessageService> logger) : BackgroundService
+    {
+        //Publisher loglanabiliyor
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var correlationId = Guid.NewGuid();
+
+            int i = 0;
+
+            while (true)
+            {
+                ExampleMessage message = new()
+                {
+                    Text = $"{++i} .mesaj"
+                };
+
+                Trace.CorrelationManager.ActivityId = correlationId;
+                logger.LogDebug("Publisher Log");
+
+                await Console.Out.WriteLineAsync($"{JsonSerializer.Serialize(message)} - Correlation Id : {correlationId}");
+
+                await publishEndpoint.Publish(message, async context =>
+                {
+                    context.Headers.Set("CorrelationId", correlationId);
+                });
+
+                await Task.Delay(750);
+            }
+        }
+    }
+}
