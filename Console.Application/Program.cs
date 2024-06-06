f9e5a64f2f46cf74a5229aa63ceb6d5b7f36ca19
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-06-06 11:46:45 +0300

    Add project files.

---
+++ Console.Application/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using NLog;
+using System.Diagnostics;
+
+Logger logger = LogManager.GetCurrentClassLogger();
+Trace.CorrelationManager.ActivityId = Guid.NewGuid();
+
+
+Work1(); 
+void Work1()
+{
+
+    Console.WriteLine("Work1 started.");
+    logger.Debug("Work1 started.");
+
+    Work2();
+}
+void Work2()
+{
+    Console.WriteLine("Work2 started.");
+    logger.Debug("Work2 started.");
+    
+    Work3();
+}
+void Work3()
+{
+    Console.WriteLine("Work3 started.");
+    logger.Debug("Work3 started.");
+}
